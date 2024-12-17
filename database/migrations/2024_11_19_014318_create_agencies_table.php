c8c1f783661d33ea808244d3485abaf8f4524979
duypv <duypv@nal.vn>
2024-12-17 09:09:42 +0700

    13

--- database/migrations/2024_11_19_014318_create_agencies_table.php
+++ database/migrations/2024_11_19_014318_create_agencies_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('agencies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone')->unique();
-            $table->string('address')->unique();
-            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
